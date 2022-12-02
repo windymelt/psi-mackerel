de0542939a748596878d9c400d3790f500fb47ac
Windymelt <windymelt@3qe.us>
2022-12-03 00:09:16 +0900

    give api key for PSI

--- build.sbt
+++ build.sbt
@@ -20,6 +20,7 @@ lazy val root = project
       "org.http4s" %%% "http4s-curl" % "0.1.1",
       "org.http4s" %%% "http4s-circe" % https4sVersion,
       "com.monovore" %%% "decline" % "2.4.0",
+      "com.monovore" %%% "decline-effect" % "2.4.0",
       "com.github.nscala-time" %% "nscala-time" % "2.32.0",
       "org.scalameta" %% "munit" % "0.7.29" % Test,
     ),
--- src/main/scala/com/github/windymelt/psimackerel/Main.scala
+++ src/main/scala/com/github/windymelt/psimackerel/Main.scala
@@ -1,16 +1,22 @@
 package com.github.windymelt.psimackerel
 
-import cats.effect.{IOApp, IO, ResourceIO, Resource}
+import cats.effect.{IOApp, IO, ResourceIO, Resource, ExitCode}
 import org.http4s.Uri
 import org.http4s.curl.CurlApp
 import org.http4s.client.Client
+import com.monovore.decline._
+import com.monovore.decline.effect._
 
-object Main extends cats.effect.IOApp with CurlApp:
-  def run(args: List[String]) =
-    val uri = Uri.fromString("https://www.3qe.us").toOption.get
-    given client: Client[IO] = curlClient
-    for
-      score <- Util.backgroundIndicator("Fetching PSI score...") use { _ => PSI().fetchPsiScore(uri) }
-      _ <- IO.println(s"ok: $score")
-    yield cats.effect.ExitCode(0)
+object Main extends CommandIOApp(name = "psi-mackerel", header = "Post Google Page Speed Insights score to Mackerel", version = "0.1.0")
+    with CurlApp:
+  val apiKeyForPsi = Opts.option[String]("psi-api-key", metavar = "API_KEY_FOR_PSI", help = "Optional: Set PSI API key (can get yours at https://developers.google.com/speed/docs/insights/v5/get-started). API may return 429 Too Many Requests error unless you give no API key.").orNone
+  override def main: Opts[IO[ExitCode]] =
+    apiKeyForPsi map { psiKey =>
+      val uri = Uri.fromString("https://www.3qe.us").toOption.get
+      given client: Client[IO] = curlClient
+      for
+        score <- Util.backgroundIndicator("Fetching PSI score...") use { _ => PSI().fetchPsiScore(uri, psiKey) }
+        _ <- IO.println(s"ok: $score")
+      yield cats.effect.ExitCode(0)
+    }
 
