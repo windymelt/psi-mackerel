309c21a4d734619e888927bd588c52d3d48ef163
Windymelt <windymelt@3qe.us>
2022-12-02 23:40:53 +0900

    apikey and scaladoc

--- src/main/scala/com/github/windymelt/psimackerel/PSI.scala
+++ src/main/scala/com/github/windymelt/psimackerel/PSI.scala
@@ -4,12 +4,24 @@ import cats.effect.IO
 import io.circe.Json
 
 class PSI():
-  def fetchPsiScore(targetUri: org.http4s.Uri)(using client: org.http4s.client.Client[IO]): IO[Option[Double]] =
+  /**
+    * Fetches Page Speed Insight Score.
+    *
+    * @param targetUri
+    * @param apiKey
+    * @param client
+    * @return Score [0, 1]
+    */
+  def fetchPsiScore(targetUri: org.http4s.Uri, apiKey: Option[String] = None)(using client: org.http4s.client.Client[IO]): IO[Option[Double]] =
     import io.circe.syntax._
     import org.http4s.circe._
     import cats.syntax.applicative.catsSyntaxApplicativeId
     import io.circe.Encoder.encodeString
-    val jIo: IO[Json] = client.expect[Json](s"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=$targetUri")
+    val url = apiKey match
+      case Some(key) => s"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=$key&url=$targetUri"
+      case None => s"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=$targetUri"
+
+    val jIo: IO[Json] = client.expect[Json](url)
     for {
       j <- jIo
       scoreJson <- (for {
