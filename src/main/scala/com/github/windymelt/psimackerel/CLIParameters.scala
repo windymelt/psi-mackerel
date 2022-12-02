522b16b55e90d997a21ed424c78debbb05bfd012
Windymelt <windymelt@3qe.us>
2022-12-03 03:31:22 +0900

    scalafmt

--- src/main/scala/com/github/windymelt/psimackerel/CLIParameters.scala
+++ src/main/scala/com/github/windymelt/psimackerel/CLIParameters.scala
@@ -8,5 +8,21 @@ object CLIParameters:
   case class Config(psiKey: Option[String], mackerelKey: Option[String])
 
   val config = (apiKeyForPsi, apiKeyForMackerel).mapN(Config.apply)
-  val apiKeyForPsi = Opts.option[String]("psi-api-key", metavar = "API_KEY_FOR_PSI", help = "Optional: Set PSI API key (can get yours at https://developers.google.com/speed/docs/insights/v5/get-started). API may return 429 Too Many Requests error unless you give no API key.").orNone
-  val apiKeyForMackerel = Opts.option[String]("mackerel-api-key", short = "k", metavar = "API_KEY_FOR_MACKEREL", help = "Set Mackerel API key").orNone
+
+  val apiKeyForPsi = Opts
+    .option[String](
+      "psi-api-key",
+      metavar = "API_KEY_FOR_PSI",
+      help = "Optional: Set PSI API key (can get yours at https://developers.google.com/speed/docs/insights/v5/get-started)." +
+        "API may return 429 Too Many Requests error unless you give no API key."
+    )
+    .orNone
+
+  val apiKeyForMackerel = Opts
+    .option[String](
+      "mackerel-api-key",
+      short = "k",
+      metavar = "API_KEY_FOR_MACKEREL",
+      help = "Set Mackerel API key"
+    )
+    .orNone
