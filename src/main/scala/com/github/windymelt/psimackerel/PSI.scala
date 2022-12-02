c2efbee8ae43fc2f9c9f400e705b39926ef231ad
Windymelt <windymelt@3qe.us>
2022-12-03 03:32:58 +0900

    scalafmt

--- src/main/scala/com/github/windymelt/psimackerel/PSI.scala
+++ src/main/scala/com/github/windymelt/psimackerel/PSI.scala
@@ -4,22 +4,26 @@ import cats.effect.IO
 import io.circe.Json
 
 class PSI():
-  /**
-    * Fetches Page Speed Insight Score.
+  /** Fetches Page Speed Insight Score.
     *
     * @param targetUri
     * @param apiKey
     * @param client
-    * @return Score [0, 1]
+    * @return
+    *   Score [0, 1]
     */
-  def fetchPsiScore(targetUri: org.http4s.Uri, apiKey: Option[String] = None)(using client: org.http4s.client.Client[IO]): IO[Option[Double]] =
+  def fetchPsiScore(targetUri: org.http4s.Uri, apiKey: Option[String] = None)(
+      using client: org.http4s.client.Client[IO]
+  ): IO[Option[Double]] =
     import io.circe.syntax._
     import org.http4s.circe._
     import cats.syntax.applicative.catsSyntaxApplicativeId
     import io.circe.Encoder.encodeString
     val url = apiKey match
-      case Some(key) => s"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=$key&url=$targetUri"
-      case None => s"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=$targetUri"
+      case Some(key) =>
+        s"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=$key&url=$targetUri"
+      case None =>
+        s"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=$targetUri"
 
     val jIo: IO[Json] = client.expect[Json](url)
     for {
